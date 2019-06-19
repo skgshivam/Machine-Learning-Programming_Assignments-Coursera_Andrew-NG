5f2656c13f69494a9f04b104f98e642ba358aa72
Shivam Gupta <shivamguptahdr98@gmail.com>
2019-06-19 19:30:18 +0530

    predict completed

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,7 +21,16 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a1 = [ones(m, 1) X];
 
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+
+a2 = [ones(m, 1) a2];
+z3 = a2 * Theta2';
+hypo = sigmoid(z3);
+[probability indices] = max(hypo');
+p = indices';
 
 
 
