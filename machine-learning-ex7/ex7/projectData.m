201ba0c81ead281171da266e11129b4e751c99be
Shivam Gupta <shivamguptahdr98@gmail.com>
2019-07-07 23:06:47 +0530

    projectData.m

--- machine-learning-ex7/ex7/projectData.m
+++ machine-learning-ex7/ex7/projectData.m
@@ -19,6 +19,15 @@ Z = zeros(size(X, 1), K);
 %
 
 
+for i=1:size(X, 1),
+    for j=1:K,
+        x = X(i, :)';
+        projection_k = x' * U(:, j);
+        Z(i, j) = projection_k;
+    end
+end
+
+
 
 
 % =============================================================
